9813eadbaaa3fbb4c4730e8bd2ff17fa76ce42b3
Yauheni Anikhouski <e.anikhouski@openmygame.com>
2026-01-26 08:03:06 +0100

    fix

--- frontend/src/metabase/querying/notebook/components/MetricsPickerStep/MetricsPickerStep.tsx
+++ frontend/src/metabase/querying/notebook/components/MetricsPickerStep/MetricsPickerStep.tsx
@@ -1,10 +1,8 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { t } from "ttag";
 
-import { collectionApi } from "metabase/api";
 import { MiniPicker } from "metabase/common/components/Pickers/MiniPicker";
 import type { MiniPickerPickableItem } from "metabase/common/components/Pickers/MiniPicker/types";
-import { useDispatch } from "metabase/lib/redux";
 import { Icon, TextInput } from "metabase/ui";
 import * as Lib from "metabase-lib";
 
@@ -28,77 +26,19 @@ export function MetricsPickerStep({
     [query, stageIndex],
   );
 
-  const dispatch = useDispatch();
-
-  const { metricsById, metricCollectionIds } = useMemo(() => {
+  const metricsById = useMemo(() => {
     const metricMap = new Map<string, Lib.MetricMetadata>();
-    const collectionIds = new Set<number>();
     metrics.forEach((metric) => {
       const displayInfo = Lib.displayInfo(query, stageIndex, metric);
       const info = displayInfo as unknown as {
         id?: number;
-        collectionId?: number | null;
       };
       if (info.id != null) {
         metricMap.set(String(info.id), metric);
-        if (info.collectionId != null) {
-          collectionIds.add(info.collectionId);
-        }
       }
     });
-    return { metricsById: metricMap, metricCollectionIds: collectionIds };
+    return metricMap;
   }, [metrics, query, stageIndex]);
-
-  // Fetch collections to get full hierarchy (location paths)
-  const [validCollectionIds, setValidCollectionIds] = useState<Set<string>>(
-    new Set(),
-  );
-
-  useEffect(() => {
-    if (metricCollectionIds.size === 0) {
-      setValidCollectionIds(new Set());
-      return;
-    }
-
-    const fetchCollectionHierarchy = async () => {
-      const allCollectionIds = new Set<string>();
-
-      // Fetch each collection to get its location
-      const collectionPromises = Array.from(metricCollectionIds).map(
-        async (collectionId) => {
-          try {
-            const collection = await dispatch(
-              collectionApi.endpoints.getCollection.initiate({
-                id: collectionId,
-              }),
-            ).unwrap();
-
-            // Add the collection itself
-            allCollectionIds.add(String(collectionId));
-
-            // Parse location to get ancestor collection IDs
-            // Location format: "/1/5/10/" means ancestors are 1, 5, 10
-            const location =
-              collection.effective_location || collection.location;
-            if (location) {
-              const ancestorIds = location
-                .split("/")
-                .filter((id: string) => id && id !== "");
-              ancestorIds.forEach((id: string) => allCollectionIds.add(id));
-            }
-          } catch {
-            // If fetch fails, just add the collection ID
-            allCollectionIds.add(String(collectionId));
-          }
-        },
-      );
-
-      await Promise.all(collectionPromises);
-      setValidCollectionIds(allCollectionIds);
-    };
-
-    fetchCollectionHierarchy();
-  }, [metricCollectionIds, dispatch]);
 
   const handleMetricSelect = useCallback(
     (item: MiniPickerPickableItem) => {
@@ -158,15 +98,11 @@ export function MetricsPickerStep({
               if (isMetric) {
                 return !metricsById.has(String(modelItem.id));
               }
+              // Always show collections for navigation
               if (modelItem.model === "collection") {
-                // Always show root collection if we have any valid metrics
-                if (modelItem.id === "root" && metricsById.size > 0) {
-                  return false;
-                }
-                // Hide collections that don't contain valid metrics
-                return !validCollectionIds.has(String(modelItem.id));
+                return false;
               }
-              // Hide all other entity types
+              // Hide all other entity types (questions, dashboards, etc.)
               return true;
             }
             return false;
